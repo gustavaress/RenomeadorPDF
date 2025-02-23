0144fa4dd0004def77ae106051c0e8c7788a855d
Gustavo Tavares <kyeldrawings@gmail.com>
2025-02-23 02:28:52 -0300

    Commit 23/02/2025 02:28:22

--- Leitor-PDFs/pdfv2.py
+++ Leitor-PDFs/pdfv2.py
@@ -43,7 +43,7 @@ arquivos = os.listdir(caminho_pasta)
 for arquivo in arquivos:
     # Aqui defino o "if" para iniciar quando o começo do nome do arquivo tiver a palavra ou frase que foi definida no inicio do código
     if arquivo.startswith(inicioArquivo):
-        # Usamos a função os.path.join para que não tenhamos que digitar /, ja que ela pode variar dependendo do sistema operacional. ela vai juntar o caminho_pasta com o arquivo, algo como C:/CaminhoArquivo/arquivo.pdf. isso vai definir para o python o caminho de cada arquivo dentro daquela pasta, desde que todos iniciem com o nome "Zezin".
+        # Usamos a função os.path.join para que não tenhamos que digitar /, ja que ela pode variar dependendo do sistema operacional. ela vai juntar o caminho_pasta com o arquivo, algo como C:/CaminhoArquivo/arquivo.pdf. isso vai definir para o python o caminho de cada arquivo dentro daquela pasta, desde que todos iniciem com o nome definido na váriavel.
         caminho_arquivo = os.path.join(caminho_pasta, arquivo)
 
         # O try não é necessario, mas foi adicionado para que possa nos retornar mensagens de erro ou sucesso no console. Sem eles, qualquer erro iria interromper a execução do codigo, garantindo que se algo de errado, o codigo não vá para pqp. No fim, usamos ele junto do except para que o except possa capturar erros, assim caso tenhamos algum erro, seremos redirecionados para parte do except, aonde nos apresentara um erro
@@ -55,7 +55,7 @@ for arquivo in arquivos:
                 
             # Se tiver nome no arquivo
             if nome:
-                # Criar novo nome para o arquivo. O novo nome sera igual ao nome extraido e a frase final de "Termo Bla bla bla.pdf"
+                 # Criar novo nome para o arquivo. O novo nome sera igual ao nome extraido e a frase final de "Termo de Equipamento -2025.pdf"
                 novo_nome = f"{nome} Termo Onboarding 2025.pdf"
                 # Define o novo caminho do arquivo. Vamos usar a mesma regra de antes (os.path.join para evitar incompatibilidade com diferentes OS, ja que ele une os dois atributos automaticamente e adiciona a / ou \ dependendo do OS). Após isso, definiremos o caminho, que sera o caminho_pasta + nome novo do arquivo, nos retornando algo como C:/CaminhoArquivo/aquivoNOVO.pdf. Esse passo é importante para dizer para o sistema operacional aonde ele deve salvar o nosso arquivo quando ele for renomeado.É OBRIGATORIO conter o nome novo do arquivo apos o caminho_pasta
                 novo_caminho_arquivo = os.path.join(caminho_pasta, novo_nome)
